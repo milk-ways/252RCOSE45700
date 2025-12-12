c7e290461193a5e06c947a806a4a32d632665052
milk-ways <joming1418@gmail.com>
2025-12-12 14:53:40 +0900

    로비씬 다대다 추가

--- Assets/Scripts/UI/LobbyUI.cs
+++ Assets/Scripts/UI/LobbyUI.cs
@@ -9,6 +9,11 @@ using System;
 public class LobbyUI : MonoBehaviour
 {
     [SerializeField] Button SingleMatchButton;
+    [SerializeField] Button MultiMatchButton;
+    [SerializeField] Button previousButton;
+    [SerializeField] Button nextButton;
+    [SerializeField] TextMeshProUGUI characterName;
+    [SerializeField] TextMeshProUGUI description;
 
     [SerializeField] RectTransform MatchmakingWaitingPanel;
     [SerializeField] TextMeshProUGUI waitingText;
@@ -16,48 +21,81 @@ public class LobbyUI : MonoBehaviour
     [SerializeField] CharacterDesc characterDesc;
 
     [SerializeField] Button QuitButton;
-    [SerializeField] TMP_InputField CustomLobbyCode;
 
+    private int current = 0;
+    private int previous = 0;
+
+    public Transform rootPlayer;
+    public Transform[] players;
 
     private void Start()
     {
         SingleMatchButton.onClick.AddListener(() =>
         {
             MatchButton();
-            if(CustomLobbyCode.text.Length > 0)
-            {
-                NetworkRunnerHandler.Instance.FindOneVsOneMatch(GetLobbyName());
-            }
-            else
-            {
-                NetworkRunnerHandler.Instance.FindOneVsOneMatch();
-            }
+            NetworkRunnerHandler.Instance.FindOneVsOneMatch();
+        });
+
+        MultiMatchButton.onClick.AddListener(() =>
+        {
+            MatchButton();
+            NetworkRunnerHandler.Instance.FindTwoVsTwoMatch();
         });
 
         QuitButton.onClick.AddListener(() =>
         {
             Application.Quit();
         });
+
+        previousButton.onClick.AddListener(PreviousPlayer);
+        nextButton.onClick.AddListener(NextPlayer);
+
+        players = new Transform[rootPlayer.childCount];
+
+        for (int i = 0; i < rootPlayer.childCount; i++)
+        {
+            players[i] = rootPlayer.GetChild(i);
+        }
+
+        SetText();
+        players[current].gameObject.SetActive(true);
     }
 
     public void MatchButton()
     {
-        SoundManager.Instance.LobbyName = GetLobbyName();
         StartCoroutine(WatingTextRoutine());
         MatchmakingWaitingPanel.gameObject.SetActive(true);
-        NetworkRunnerHandler.Instance.SelectedPlayer = 0;
+        NetworkRunnerHandler.Instance.SelectedPlayer = current;
     }
 
-    public string GetLobbyName()
+    public void NextPlayer()
     {
-        if(CustomLobbyCode.text.Length > 6)
-        {
-            return CustomLobbyCode.text.Substring(0, 6);
-        }
-        else
-        {
-            return CustomLobbyCode.text;
-        }
+        players[current].gameObject.SetActive(false);
+
+        previous = current;
+        current = (current + 1) % rootPlayer.childCount;
+
+        SetText();
+
+        players[current].gameObject.SetActive(true);
+    }
+
+    public void PreviousPlayer()
+    {
+        players[current].gameObject.SetActive(false);
+
+        current = previous;
+        previous = previous == 0 ? rootPlayer.childCount - 1 : previous - 1;
+
+        SetText();
+
+        players[current].gameObject.SetActive(true);
+    }
+
+    public void SetText()
+    {
+        characterName.text = characterDesc.characterName[current];
+        description.text = characterDesc.characterDesc[current];
     }
 
     private float duration = 0.2f;
@@ -67,7 +105,7 @@ public class LobbyUI : MonoBehaviour
         StringBuilder builder = new();
         string tot = "技记 立加 吝....";
         int baseLen = "技记 立加 吝".Length;
-        while(true)
+        while (true)
         {
             waitingText.text = tot.Substring(0, baseLen + len);
             yield return new WaitForSeconds(duration);
